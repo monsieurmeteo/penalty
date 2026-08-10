0cf12df9a363ed6d01865a6de1c5c9acb8defa0f
Gregory Langlet <langlet.gregory@gmail.com>
2026-08-10 11:17:12 +0200

    chore: remove temp fix script

--- scripts/fix_pen_filter.py
+++
@@ -1,7 +0,0 @@
-﻿lines = open("scripts/auto_premium_unibet.py", encoding="utf-8").readlines()
-old = lines[658]
-new = old.replace('pen_per_match", 0) > 0', 'ref_name", "Inconnu") != "Inconnu"')
-lines[658] = new
-open("scripts/auto_premium_unibet.py", "w", encoding="utf-8").writelines(lines)
-lines2 = open("scripts/auto_premium_unibet.py", encoding="utf-8").readlines()
-print("Line 659:", lines2[658].rstrip())
